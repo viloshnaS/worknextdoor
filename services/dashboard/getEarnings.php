f759c8fcda2e5ab7d43db7a197dbb266543dab3e
houshujun <33526523+houshujun@users.noreply.github.com>
2018-07-18 17:09:44 +0200

    getHubEarning

--- services/dashboard/getEarnings.php
+++ services/dashboard/getEarnings.php
@@ -10,16 +10,27 @@ if(isset($_GET['user_id']) && !empty($_GET['user_id'])) {
     
  }
 
-
+//echo getEarnings(2, 3);
  
 function getEarnings($id, $interval){
 	global $connection;
 
-	$sql= "SELECT SUM(DATEDIFF(b.booking_date_end,b.booking_date_start) * unit_price) AS TOTAL_EARNINGS 
-	FROM booking b, hubs h, spaces s
-	WHERE h.hub_id = s.hub_id 
-	AND s.space_id = b.space_id
-	AND h.user_id = $id";
+
+	$sql= "SELECT h.name as name,  
+                sum((CASE 
+                WHEN package_id = 1 THEN
+                               timestampdiff(hour, b.booking_date_start,b.booking_date_end)
+                WHEN package_id = 2 THEN
+                               timestampdiff(day, b.booking_date_start,b.booking_date_end) + 1
+                WHEN package_id = 3 THEN
+                               timestampdiff(month, b.booking_date_start,b.booking_date_end)
+                END)*unit_price )AS Total_Earning
+                FROM booking b, hubs h, spaces s
+                WHERE h.hub_id = s.hub_id 
+                AND s.space_id = b.space_id
+                AND h.user_id = $id";
+
+
 
 	if ($interval=='1') {  
 	$sql = $sql . " AND b.booking_date_start BETWEEN NOW() - INTERVAL 7 DAY AND NOW()";
@@ -35,8 +46,11 @@ function getEarnings($id, $interval){
 	$sql = $sql . " AND b.booking_date_start BETWEEN NOW() - INTERVAL 365 DAY AND NOW()";
 	}
 
-	echo $sql;
+	$sql = $sql . " GROUP BY h.name,h.hub_id";
+
+	
 	$result = $connection->query($sql);
+	$rows = array();
 
 	if ($result->num_rows > 0) {
 	    // output data of each row
@@ -52,4 +66,4 @@ function getEarnings($id, $interval){
 	return json_encode($rows);
 }
 
-?>s
+?>
