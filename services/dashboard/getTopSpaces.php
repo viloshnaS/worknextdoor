f916e9df72ad0dfa6e9fc753f8c5b1b28427f4e2
oluchi <luchblingz@yahoo.com>
2018-07-18 10:46:59 +0200

    getTopSpaces

---
+++ services/dashboard/getTopSpaces.php
@@ -0,0 +1,59 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+require('../../connection.php');
+$connection = $conn;
+
+if(isset($_GET['user_id']) && !empty($_GET['user_id'])) {
+    $user_id = $_GET['user_id'];
+    $interval= $_GET['interval'];
+    echo getBookings($user_id, $interval);
+    
+ }
+
+
+ 
+function getBookings($id, $interval){
+	global $connection;
+
+	$sql= "SELECT s.space_name, COUNT(*) AS TOTAL_BOOKINGS
+	FROM booking b, hubs h, spaces s
+	WHERE h.hub_id = s.hub_id 
+	AND s.space_id = b.space_id
+	AND h.user_id = $id";
+
+	if ($interval=='1') {  
+	$sql = $sql . " AND b.booking_date_start BETWEEN NOW() - INTERVAL 7 DAY AND NOW()";
+
+	}
+
+
+	if ($interval=='2') {  
+	$sql = $sql . " AND b.booking_date_start BETWEEN NOW() - INTERVAL 30 DAY AND NOW()";
+	}
+
+
+	if ($interval=='3') {  
+	$sql = $sql . " AND b.booking_date_start BETWEEN NOW() - INTERVAL 365 DAY AND NOW()";
+	}
+
+	$sql = $sql . " GROUP BY s.space_name
+    ORDER BY TOTAL_BOOKINGS DESC";
+	echo $sql;
+	$result = $connection->query($sql);
+
+	if ($result->num_rows > 0) {
+	    // output data of each row
+	    while($row = $result->fetch_assoc()) {
+	    	
+	        $rows[] = $row;
+
+
+	    }
+	} else {
+	    echo "";
+	}
+
+	return json_encode($rows);
+}
+
+?>
